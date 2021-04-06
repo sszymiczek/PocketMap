6e19a957d1edd4d58a61ec78fb3807a8dfac3f29
ajamrozy <jamani@tlen.pl>
2021-04-06 23:50:20 +0200

    Dodanie przycisków do mapy

--- src/main/java/GraphicsDemo2.java
+++ src/main/java/GraphicsDemo2.java
@@ -4,36 +4,23 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
 
-public class GraphicDemo extends JPanel {
+public class GraphicsDemo2 extends JFrame {
+    private JPanel panel1 = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
     private static int SQUARE_WIDTH = 20;
-    private Graphics2D graphic2D;
     private Random random = new Random();
 
-    public void paintComponent(Graphics graphic) {
-        super.paintComponent(graphic);
-        this.setBackground(Color.WHITE);
-        this.graphic2D = (Graphics2D) graphic;
-        random.setSeed(2137);
-        fillWithSquares(500, 500);
-    }
-
-    public void fillWithSquares(int xPixels, int yPixels){
+    public JPanel fillWithSquares(int xPixels, int yPixels, JPanel panel1){
         int maxX = (int) xPixels / SQUARE_WIDTH;
         int maxY = (int) yPixels / SQUARE_WIDTH;
 
         for (int i = 0; i < maxX; i++) {
             for (int j = 0; j < maxY; j++) {
-                paintAtCoordinates(i, j, randomColor());
-
+                //paintAtCoordinates(i, j, randomColor());
+                panel1.add(paintButton(randomColor(), i, j));
             }
         }
+        return panel1;
     }
-
-    public void paintAtCoordinates(int x, int y, Color color){
-        this.graphic2D.setColor(color);
-        graphic2D.fillRect(SQUARE_WIDTH * x, SQUARE_WIDTH * y, SQUARE_WIDTH, SQUARE_WIDTH);
-    }
-
     public JButton paintButton(Color color, int x, int y){
         JButton button = new JButton();
         button.setBackground(color);
@@ -62,4 +49,16 @@ public class GraphicDemo extends JPanel {
         };
     }
 
+    public GraphicsDemo2(){
+        this.setSize(500, 500);
+        this.setVisible(true);
+        random.setSeed(2137);
+        panel1 = fillWithSquares(2000, 2000, panel1);
+        this.add(panel1);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        GraphicsDemo2 test = new GraphicsDemo2();
+    }
 }
