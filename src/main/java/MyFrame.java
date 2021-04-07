8850a5e104f7b88f75efb487ce260bac1364d10f
Szymik <szymonszymiczek32@gmail.com>
2021-04-07 13:19:18 +0200

    debug

--- src/main/java/MyFrame.java
+++ src/main/java/MyFrame.java
@@ -1,13 +1,13 @@
-import javax.swing.*;
-
-public class MyFrame extends JFrame{
-
-    GraphicDemo graphicDemo = new GraphicDemo();
-
-    public MyFrame(){
-        this.setSize(500, 500);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.add(graphicDemo);
-        this.setVisible(true);
-    }
-}
+//import javax.swing.*;
+//
+//public class MyFrame extends JFrame{
+//
+//    GraphicDemo graphicDemo = new GraphicDemo();
+//
+//    public MyFrame(){
+//        this.setSize(500, 500);
+//        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        this.add(graphicDemo);
+//        this.setVisible(true);
+//    }
+//}
