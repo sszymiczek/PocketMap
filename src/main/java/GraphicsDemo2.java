9b4654f4457473732fe45c29f90bb82db578e29a
Szymik <szymonszymiczek32@gmail.com>
2021-04-07 13:17:08 +0200

    refactoring

--- src/main/java/GraphicsDemo2.java
+++ src/main/java/GraphicsDemo2.java
@@ -8,9 +8,20 @@ import java.util.HashMap;
 import java.util.Random;
 
 public class GraphicsDemo2 extends JFrame {
-    private JPanel panel1 = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
+    private JPanel jPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
     private static int SQUARE_WIDTH = 50;
+
     private Random random = new Random();
+    private HashMap<String, ImageIcon> mapsOfIcons= new HashMap<>();
+
+    public GraphicsDemo2(){
+        loadIcons();
+        this.setVisible(true);
+        jPanel = fillWithSquares(2000, 2000, jPanel);
+        this.add(jPanel);
+        this.setSize(500, 500);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
     public JPanel fillWithSquares(int xPixels, int yPixels, JPanel panel1){
         int maxX = (int) xPixels / SQUARE_WIDTH;
@@ -18,12 +29,13 @@ public class GraphicsDemo2 extends JFrame {
 
         for (int i = 0; i < maxX; i++) {
             for (int j = 0; j < maxY; j++) {
-                panel1.add(paintButton(randomIcon(), i, j));
+                panel1.add(paintedButton(randomIcon(), i, j));
             }
         }
         return panel1;
     }
-    public JButton paintButton(Icon icon, int x, int y){
+
+    public JButton paintedButton(Icon icon, int x, int y){
         JButton button = new JButton();
         button.setIcon(icon);
         button.setOpaque(true);
@@ -42,58 +54,39 @@ public class GraphicsDemo2 extends JFrame {
         return button;
     }
 
-    public Color randomColor(){
-        int rnd = random.nextInt(4);
-        return switch (rnd) {
-            case 0 -> Color.BLUE;
-            case 1 -> Color.RED;
-            case 2 -> Color.GREEN;
-            case 3 -> Color.YELLOW;
-            default -> Color.BLACK;
-        };
-    }
-
     public ImageIcon randomIcon(){
         int rnd = random.nextInt(4);
-        String iconName = null;
-        switch (rnd) {
-            case 0 -> iconName = "0.png";
-            case 1 -> iconName = "1.png";
-            case 2 -> iconName = "2.png";
-            case 3 -> iconName = "4.png";
-        }
-        Path chosenPath = Paths.get(System.getProperty("user.home"),"icons", iconName);
-        return new ImageIcon(String.valueOf(chosenPath));
 
+        switch (rnd) {
+            case 0 -> {
+                return this.mapsOfIcons.get("green");
+            }
+            case 1 -> {
+                return this.mapsOfIcons.get("red");
+            }
+            case 2 -> {
+                return this.mapsOfIcons.get("blue");
+            }
+            case 3 -> {
+                return this.mapsOfIcons.get("yellow");
+            }
+            default -> {
+                return this.mapsOfIcons.get("blue");
+            }
+        }
     }
 
-    public HashMap<String, ImageIcon> loadIcon(){
-        HashMap<String, ImageIcon> mapsOfIcons= new HashMap<>();
+    public void loadIcons(){
+        this.mapsOfIcons.clear();
 
         ImageIcon green = new ImageIcon(String.valueOf(Paths.get(System.getProperty("user.home"),"icons", "0.png")));
         ImageIcon red = new ImageIcon(String.valueOf(Paths.get(System.getProperty("user.home"),"icons", "1.png")));
         ImageIcon blue = new ImageIcon(String.valueOf(Paths.get(System.getProperty("user.home"),"icons", "2.png")));
         ImageIcon yellow = new ImageIcon(String.valueOf(Paths.get(System.getProperty("user.home"),"icons", "4.png")));
 
-
-        mapsOfIcons.put("green", green);
-        mapsOfIcons.put("red", red);
-        mapsOfIcons.put("blue", blue);
-        mapsOfIcons.put("yellow", yellow);
-
-        return mapsOfIcons;
-    }
-
-    public GraphicsDemo2(){
-        this.setVisible(true);
-        panel1 = fillWithSquares(2000, 2000, panel1);
-        this.add(panel1);
-        this.setSize(500, 500);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    }
-
-    public static void main(String[] args) {
-        GraphicsDemo2 test = new GraphicsDemo2();
+        this.mapsOfIcons.put("green", green);
+        this.mapsOfIcons.put("red", red);
+        this.mapsOfIcons.put("blue", blue);
+        this.mapsOfIcons.put("yellow", yellow);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        MyFrame myFrame = new MyFrame();
-
+        GraphicsDemo2 test = new GraphicsDemo2();
     }
 }
