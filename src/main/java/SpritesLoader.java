f3ac5f71587def7d45cdc4a442f2fe3f8715f6fb
Szymik <szymonszymiczek32@gmail.com>
2021-05-18 23:55:23 +0200

    more colours added

--- src/main/java/SpritesLoader.java
+++ src/main/java/SpritesLoader.java
@@ -14,17 +14,28 @@ public abstract class SpritesLoader {
     public static void loadMapSprites() {
         mapSprites.clear();
 
-        ImageIcon green = new ImageIcon("src/main/resources/iconsMap/green.png");
-        ImageIcon red = new ImageIcon("src/main/resources/iconsMap/red.png");
-        ImageIcon blue = new ImageIcon("src/main/resources/iconsMap/blue.png");
-        ImageIcon yellow = new ImageIcon("src/main/resources/iconsMap/yellow.png");
-        ImageIcon black = new ImageIcon("src/main/resources/iconsMap/black.png");
+        mapSprites.put("green",
+                new ImageIcon("src/main/resources/iconsMap/green.png"));
+        mapSprites.put("lightGreen",
+                new ImageIcon("src/main/resources/iconsMap/lightGreen.png"));
+        mapSprites.put("darkGreen",
+                new ImageIcon("src/main/resources/iconsMap/darkGreen.png"));
 
-        mapSprites.put("green", green);
-        mapSprites.put("red", red);
-        mapSprites.put("blue", blue);
-        mapSprites.put("yellow", yellow);
-        mapSprites.put("black", black);
+        mapSprites.put("red",
+                new ImageIcon("src/main/resources/iconsMap/red.png"));
+
+        mapSprites.put("blue",
+                new ImageIcon("src/main/resources/iconsMap/blue.png"));
+        mapSprites.put("lightBlue",
+                new ImageIcon("src/main/resources/iconsMap/lightBlue.png"));
+        mapSprites.put("darkBlue",
+                new ImageIcon("src/main/resources/iconsMap/darkBlue.png"));
+
+        mapSprites.put("yellow",
+                new ImageIcon("src/main/resources/iconsMap/yellow.png"));
+        mapSprites.put("black",
+                new ImageIcon("src/main/resources/iconsMap/black.png"));
+
     }
 
     public static void loadArrowSprites() {
